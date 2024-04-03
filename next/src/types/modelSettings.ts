daa62f0078622876f7a89c6d7bc9ff7493bdc5a7
Tesla2678 <71001519+Tesla2678@users.noreply.github.com>
2024-04-03 15:44:23 +1100

    Update modelSettings.ts

--- next/src/types/modelSettings.ts
+++ next/src/types/modelSettings.ts
@@ -1,6 +1,6 @@
 import { type Language } from "../utils/languages";
 
-export const [GPT_35_TURBO, GPT_35_TURBO_16K, GPT_4] = [
+export const [GPT_35_TURBO, GPT_35_TURBO_16K, GPT_4, GPT_4_0125_PREVIEW] = [
   "gpt-3.5-turbo" as const,
   "gpt-3.5-turbo-16k" as const,
   "gpt-4" as const,
